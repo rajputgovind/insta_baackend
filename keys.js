62927e6a11150357824e259753053ebbe820f236
rajputgovind <govindrajput7649@gmail.com>
2024-05-06 21:47:51 +0530

    instagram backend first commit

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // mongoUrl: "mongodb+srv://FlyingPanda730:UgpngUX6DVAx1Xq0@cluster0.ormku05.mongodb.net/?retryWrites=true&w=majority",
+    mongoUrl: "mongodb+srv://govindjadam:Govind892001@cluster0.gkvgtvi.mongodb.net/Instagram_DB?authSource=admin&replicaSet=atlas-115k8o-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+    Jwt_secret: "faslkfocvneofu"
+
+};
